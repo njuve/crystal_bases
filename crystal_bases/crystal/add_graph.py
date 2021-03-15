86931823ecfb59c4c370db2a8a8c41eee5e1c2b7
njuve <ale.nnn.r@gmail.com>
2021-03-15 18:15:30 +0900

    Fix test

--- crystal_bases/crystal/add_graph.py
+++ crystal_bases/crystal/add_graph.py
@@ -1,12 +1,11 @@
 import networkx as nx
-from typing import Any, Union
-from crystal_bases.crystal.crystal_graph import CrystalGraph, crystal_graph
-from crystal_bases.young.tableau import Tableau, tableau
-from crystal_bases.young.pr import pr
+from typing import Any
+from crystal_bases.crystal.crystal_graph import CrystalGraph
+from crystal_bases.young.tableau import Tableau
 
 
 def add_graph(crystal_graph: CrystalGraph, operator: Any) -> nx.DiGraph:
-    """add graph to a crystal graph
+    """add graph stucture to a crystal graph
 
     Args:
         crystal_graph: CrystalGraph
@@ -16,23 +15,26 @@ def add_graph(crystal_graph: CrystalGraph, operator: Any) -> nx.DiGraph:
         nx.DiGraph: [description]
 
     Examples:
+        >>> from crystal_bases.crystal.crystal_graph import crystal_graph
+        >>> from crystal_bases.young.tableau import tableau
+        >>> from crystal_bases.young.pr import pr
         >>> tab = tableau(boxes=[[1, 1], [2, 2]], orientation='row')
         >>> B = crystal_graph(tab = tab, n = 3)
-        >>> add_graph(crystal_graph = B, operator = pr(n=3))
+        >>> G = add_graph(crystal_graph = B, operator = pr(n=3))
+        >>> B.G.nodes
+        NodeView((Tableau(boxes=[[2, 2], [3, 3]], orientation='row'), Tableau(boxes=[[1, 2], [2, 3]], orientation='row'), Tableau(boxes=[[1, 2], [3, 3]], orientation='row'), Tableau(boxes=[[1, 1], [2, 3]], orientation='row'), Tableau(boxes=[[1, 1], [3, 3]], orientation='row'), Tableau(boxes=[[1, 1], [2, 2]], orientation='row')))
     """
     return AddGraph().add_graph(crystal_graph=crystal_graph, operator=operator)
 
 
 class AddGraph:
-    def add_edge(self, G: nx.DiGraph, operator: Any, tab: Tableau) -> Any:
-        if operator(tab) is not None:
-            op_tab = operator(tab)
-            G.add_edge(tab)
-            return self.add_edge(G, operator, op_tab)
-        else:
-            return G
+    def add_edge(self, G: nx.DiGraph, operator: Any, tab: Tableau) -> nx.DiGraph:
+        op_tab: Tableau = operator(tab)
+        G.add_edge(operator, op_tab, i="op1")
+        return G
 
     def add_graph(self, crystal_graph: CrystalGraph, operator: Any) -> nx.DiGraph:
-        return self.add_edge(
-            G=crystal_graph.G, operator=operator, tab=crystal_graph.tab
-        )
+        G = crystal_graph.G.copy()
+        for node in crystal_graph.G.nodes:
+            G = self.add_edge(G=G, operator=operator, tab=node)
+        return G
